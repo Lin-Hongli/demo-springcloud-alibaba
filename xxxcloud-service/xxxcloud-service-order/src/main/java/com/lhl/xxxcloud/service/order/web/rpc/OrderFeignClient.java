f88f7aaa7319e945f565d174255765d4fad0592f
LHL <2381555134@qq.com>
2022-09-08 18:00:48 +0800

    项目初始化

--- xxxcloud-service/xxxcloud-service-order/src/main/java/com/lhl/xxxcloud/service/order/web/rpc/OrderFeignClient.java
+++ xxxcloud-service/xxxcloud-service-order/src/main/java/com/lhl/xxxcloud/service/order/web/rpc/OrderFeignClient.java
@@ -38,7 +38,7 @@ public class OrderFeignClient implements IOrderFeignClient {
         Order order = new Order();
         BeanUtils.copyProperties(orderDTO,order);
         orderService.save(order);
-        //int i=1/0;
+        int i=1/0;
         return Result.success();
     }
 
