526b4f64128c4c0b668c86d88a80b5b667dad555
LHL <2381555134@qq.com>
2022-09-13 15:18:59 +0800

    项目初始化

--- xxxcloud-service/xxxcloud-service-payment/src/main/java/com/lhl/xxxcloud/service/payment/web/controller/PaymentController.java
+++ xxxcloud-service/xxxcloud-service-payment/src/main/java/com/lhl/xxxcloud/service/payment/web/controller/PaymentController.java
@@ -2,11 +2,8 @@ package com.lhl.xxxcloud.service.payment.web.controller;
 
 import com.lhl.util.result.Result;
 import com.lhl.xxxcloud.service.api.order.feign.IOrderFeignClient;
-import com.lhl.xxxcloud.service.api.order.model.dto.OrderDTO;
-import com.lhl.xxxcloud.service.api.payment.feign.IPaymentFeignClient;
 import com.lhl.xxxcloud.service.payment.service.IPaymentService;
 import io.swagger.annotations.Api;
-import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
